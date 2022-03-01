6ee7e628d080e0dd924dc44693b2074aaac07590
Gio <georgesmessanges@hotmail.com>
2022-03-01 12:32:35 +0100

    ""

--- src/georges/color/model/Color.java
+++ src/georges/color/model/Color.java
@@ -1,4 +1,54 @@
 package georges.color.model;
 
+import java.awt.geom.IllegalPathStateException;
+import java.rmi.server.ExportException;
+
 public class Color {
+
+    public int rouge;
+    public int vert;
+    public int bleu;
+    public String hexa;
+
+    public Color(int rouge, int vert, int bleu){
+        this.rouge = rouge;
+        this.vert =  vert;
+        this.bleu = bleu;
+    }
+
+    public Color(String hexa){
+        this.hexa = hexa;
+    }
+
+    public void validArgRouge() {
+        if (rouge < 0 || rouge > 255) {
+            throw new IllegalArgumentException("rouge n'est pas un chiffre compris entre 0 et 255");
+        }
+    }
+
+    public void validArgVert(){
+        if(vert < 0 || vert > 255){
+            throw new IllegalArgumentException("rouge n'est pas un chiffre compris entre 0 et 255");
+        }
+    }
+
+    public void validArgBleu(){
+        if(bleu < 0 || bleu > 255){
+            throw new IllegalArgumentException("rouge n'est pas un chiffre compris entre 0 et 255");
+        }
+    }
+
+    public void validStringConstructor(){
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
--- test/georges/color/model/ColorTest.java
+++ test/georges/color/model/ColorTest.java
@@ -2,14 +2,56 @@ package georges.color.model;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class ColorTest {
+
+    private Color color;
 
     @BeforeEach
     void setUp() {
+        color = new Color(300,300,-1);
+        color = new Color("#0000FF");
     }
+
+    @Test
+    void testConstruc_3_arg(){
+        assertEquals(300, color.rouge,"valeur du constructeur non identique");
+        assertEquals(300, color.vert, "valeur du constructeur non identique");
+        assertEquals(-1, color.bleu, "valeur du constructeur non identique");
+    }
+
+    @Test
+    void testValidArg_returnIllegalAgurmentException_if_rougeNot_between0_255(){
+        assertThrows(IllegalArgumentException.class, ()->color.validArgRouge(), "l'exception du range du rouge n'est pas levée");
+    }
+    @Test
+    void testValidArg_returnIllegalAgurmentException_if_vertNot_between0_255(){
+        assertThrows(IllegalArgumentException.class, ()->color.validArgVert(), "l'exception du range du vert n'est pas levée");
+    }
+    @Test
+    void testValidArg_returnIllegalAgurmentException_if_bleuNot_between0_255(){
+        assertThrows(IllegalArgumentException.class, ()->color.validArgBleu(), "l'exception du range du bleu n'est pas levée");
+    }
+
+    @Test
+    void testConstruc_StrArg(){
+        assertEquals("#0000FF", color.hexa, "valeur du constructeur non identique");
+    }
+
+    @Test
+    void testValidStringConstruct_returnIllegalAgurmentException_if_incorrectFormat(){
+        assertThrows(IllegalArgumentException.class, () -> color.validStringConstructor(), "l'exception du format de la String n'est pas levée");
+    }
+
+
+
+
+
+
 
     @AfterEach
     void tearDown() {
