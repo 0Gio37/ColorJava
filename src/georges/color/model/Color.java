2b18e95f02753f5adde8c5df592ccebf9d579ddb
Gio <georgesmessanges@hotmail.com>
2022-03-02 11:58:50 +0100

    ""

--- src/georges/color/model/Color.java
+++ src/georges/color/model/Color.java
@@ -1,33 +1,25 @@
 package georges.color.model;
 
-import java.awt.geom.IllegalPathStateException;
-import java.rmi.server.ExportException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Color {
+
+    private final static char[] VALUES = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+            'F' };
+    private char[] tabChar;
 
     public int rouge;
     public int vert;
     public int bleu;
     public String hexa;
 
+
     public Color(int rouge, int vert, int bleu){
-        if (rouge < 0 || rouge > 255) {
-            throw new IllegalArgumentException("rouge n'est pas un chiffre compris entre 0 et 255");
-        }else{
-            this.rouge = rouge;
-        }
-        if (vert < 0 || vert > 255) {
-            throw new IllegalArgumentException("vert n'est pas un chiffre compris entre 0 et 255");
-        }else{
-            this.vert = vert;
-        }
-        if (bleu < 0 || bleu > 255) {
-            throw new IllegalArgumentException("bleu n'est pas un chiffre compris entre 0 et 255");
-        }else{
-            this.bleu = bleu;
-        }
+        setRed(rouge);
+        setGreen(vert);
+        setBlue(bleu);
+        convertRGBInHexa(rouge,vert,bleu);
     }
 
 
@@ -39,57 +31,109 @@ public class Color {
         if(!m.matches()){
             throw new IllegalArgumentException("le format hexa n'est pas le bon");
         }else{
+            convertHexaInRGB(hexa);
             this.hexa = hexa;
         }
     }
+
+    public String convertRGBInHexa(int rouge, int vert, int bleu){
+        tabChar = new char[6];
+        tabChar[0] = VALUES[rouge / 16];
+        tabChar[1] = VALUES[rouge % 16];
+        tabChar[2] = VALUES[vert / 16];
+        tabChar[3] = VALUES[vert % 16];
+        tabChar[4] = VALUES[bleu / 16];
+        tabChar[5] = VALUES[bleu % 16];
+
+        this.hexa = "#"+tabChar[0]+tabChar[1]+tabChar[2]+tabChar[3]+tabChar[4]+tabChar[5];
+        return this.hexa;
+    }
+
+    public void convertHexaInRGB(String hexa){
+        tabChar = new char[6];
+        tabChar[0] = hexa.charAt(1);
+        tabChar[1] = hexa.charAt(2);
+        tabChar[2] = hexa.charAt(3);
+        tabChar[3] = hexa.charAt(4);
+        tabChar[4] = hexa.charAt(5);
+        tabChar[5] = hexa.charAt(6);
+        upDatehexaInRGBforRed(tabChar[0], tabChar[1]);
+        upDatehexaInRGBforGreen(tabChar[2],tabChar[3]);
+        upDatehexaInRGBforBlue(tabChar[4],tabChar[5]);
+    }
+
+    private int indexOf(char charValue) {
+        for (int i = 0; i < VALUES.length; i++)
+            if (VALUES[i] == charValue)
+                return i;
+        throw new IllegalArgumentException();
+    }
+
+    public int upDatehexaInRGBforRed(char red1, char red2){
+        this.rouge = indexOf(red1) * 16 + indexOf(red2);
+        return this.rouge;
+    }
+    public int upDatehexaInRGBforGreen(char green3, char green4){
+        this.vert = indexOf(green3) * 16 + indexOf(green4);
+        return this.vert;
+    }
+    public int upDatehexaInRGBforBlue(char blue5, char blue6){
+        this.bleu = indexOf(blue5) * 16 + indexOf(blue6);
+        return this.bleu;
+    }
+
+    public boolean validRangeColor(int color){
+        if (color < 0 || color > 255) {
+            throw new IllegalArgumentException("la couleur n'est pas un chiffre compris entre 0 et 255");
+        }
+        return true;
+    }
+
 
 
     public int getRed(){
         return rouge;
     }
     public void setRed(int newColor){
-        this.rouge = newColor;
+        if(validRangeColor(newColor)){
+            this.rouge = newColor;
+        }
     }
 
     public int getGreen(){
         return vert;
     }
     public void setGreen(int newColor){
-        this.vert = newColor;
+        if(validRangeColor(newColor)){
+            this.vert = newColor;
+        }
     }
 
     public int getBlue(){
         return bleu;
     }
-
     public void setBlue(int newColor){
-        this.bleu = newColor;
+        if(validRangeColor(newColor)){
+            this.bleu = newColor;
+        }
     }
 
-  public String getHexaValue(){
+  public String getHexValue(){
         return hexa;
   }
 
-  public String setHexaValue(String newValue){
+  public String setHexValue(String newValue){
         this.hexa = newValue;
         return hexa;
   }
 
-  public String toString(){
-        return "value="+hexa;
-
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @Override
+    public String toString() {
+        return "Color{" +
+                "rouge=" + rouge +
+                ", vert=" + vert +
+                ", bleu=" + bleu +
+                ", hexa='" + hexa + '\'' +
+                '}';
+    }
 }
--- test/georges/color/model/ColorTest.java
+++ test/georges/color/model/ColorTest.java
@@ -9,39 +9,87 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ColorTest {
 
-    private Color color1;
-    private Color color2;
+    private Color color;
 
     @BeforeEach
     void setUp() {
-        color1 = new Color(100,100,45);
-        //color2 = new Color("0000FF");
+        color = new Color(50,100,200);
+        //color = new Color("#3264C8");
     }
-
-
     @AfterEach
     void tearDown() {
-        color1 = null;
-        color2 = null;
+        color = null;
     }
 
+
     @Test
-    void testConstruc_3_arg(){
+    void test_Construc_3_arg(){
         assertAll("object non conforme",
-                () -> assertEquals(100, color1.rouge, "valeur du constructeur non identique"),
-                () -> assertEquals(100, color1.vert,"valeur du constructeur non identique"),
-                () -> assertEquals(45, color1.bleu, "valeur du constructeur non identique")
+                () -> assertEquals(50, color.rouge, "valeur du constructeur non identique"),
+                () -> assertEquals(100, color.vert,"valeur du constructeur non identique"),
+                () -> assertEquals(200, color.bleu, "valeur du constructeur non identique"),
+                () -> assertEquals("#3264C8", color.hexa, "conversion hexa du constructeur non correspondant")
                 );
     }
 
     @Test
-    void testConstruc_rangeColors(){
-        assertThrows(IllegalArgumentException.class, () -> color1 = new Color(-1,200,50));
+    void testConstruc_1_arg(){
+        assertAll("object non conforme",
+                () -> assertEquals("#3264C8", color.hexa, "valeur du constructeur non identique"),
+                () -> assertEquals(50, color.rouge, "conversion RGB du constructeur non correspondant"),
+                () -> assertEquals(100, color.vert,"conversion RGB du constructeur non correspondant"),
+                () -> assertEquals(200, color.bleu, "conversion RGB du constructeur non correspondant")
+        );
     }
 
     @Test
+    void test_Method_illegalExcep_for_color_outOfRange(){
+        assertThrows(IllegalArgumentException.class, () -> color.validRangeColor(400),"Exception range non levée");
+        assertThrows(IllegalArgumentException.class, () -> color.validRangeColor(-10),"Exception range non levée");
+    }
+
+
+
+    @Test
+    void test_illegalException_if_setRedColor_out(){
+        assertAll("object non conforme",
+                () -> assertThrows(IllegalArgumentException.class, () -> color.setRed(-10),"exception non levée"),
+                () -> assertEquals(50,color.getRed(),"la valeur du rouge n'est pas récupérée"),
+                () -> assertEquals(100,color.getGreen(),"la valeur du rouge n'est pas récupérée"),
+                () -> assertEquals(200,color.getBlue(),"la valeur du rouge n'est pas récupérée")
+                );
+    }
+
+    @Test
+    void test_illegalException_if_setGreenColor_out(){
+        assertAll("object non conforme",
+                () -> assertThrows(IllegalArgumentException.class, () -> color.setGreen(-10),"exception non levée"),
+                () -> assertEquals(50,color.getRed(),"la valeur du rouge n'est pas récupérée"),
+                () -> assertEquals(100,color.getGreen(),"la valeur du rouge n'est pas récupérée"),
+                () -> assertEquals(200,color.getBlue(),"la valeur du rouge n'est pas récupérée")
+        );
+    }
+
+    @Test
+    void test_illegalException_if_setBlueColor_out(){
+        assertAll("object non conforme",
+                () -> assertThrows(IllegalArgumentException.class, () -> color.setBlue(-10),"exception non levée"),
+                () -> assertEquals(50,color.getRed(),"la valeur du rouge n'est pas récupérée"),
+                () -> assertEquals(100,color.getGreen(),"la valeur du rouge n'est pas récupérée"),
+                () -> assertEquals(200,color.getBlue(),"la valeur du rouge n'est pas récupérée")
+        );
+    }
+
+
+
+
+
+
+
+
+    @Test
     void testConstruc_hexa(){
-        assertThrows(IllegalArgumentException.class, () -> color2 = new Color("0000PP"));
+        assertThrows(IllegalArgumentException.class, () -> color = new Color("0000PP"));
     }
 
 
@@ -49,56 +97,72 @@ class ColorTest {
 
     @Test
     void testGetRed(){
-        assertEquals(300,color1.getRed(),"la valeur du rouge n'est pas récupérée");
+        assertEquals(300,color.getRed(),"la valeur du rouge n'est pas récupérée");
     }
 
     @Test
     void testSetRed() {
-        color1.setRed(400);
+        color.setRed(200);
         assertAll("objet non conforme",
-                () -> assertEquals(400, color1.getRed(), "le get du rouge apres son set n'a pas la bonne valeur "));
-        //assertEquals(400, color1.setRed(400), "la valeur du rouge n'a pas été setée");
+                () -> assertEquals(200, color.getRed(), "le get du rouge apres son set n'a pas la bonne valeur "),
+                () -> assertEquals(45, color.getBlue()),
+                () -> assertEquals(100, color.getGreen()),
+                () -> assertEquals("#C8642D",color.getHexValue())
+        );
+
+        //assertEquals(400, color.setRed(400), "la valeur du rouge n'a pas été setée");
     }
 
     @Test
+    void testSetRed_with_wrong_value_range_bigger_than_255(){
+        assertThrows(IllegalArgumentException.class, () -> color.setRed(400));
+    }
+
+    @Test
+    void testSetRed_with_wrong_value_range_smaller_than_0(){
+        assertThrows(IllegalArgumentException.class, () -> color.setRed(-1));
+    }
+
+
+    @Test
     void testGetGreen(){
-        assertEquals(500,color1.getGreen(),"la valeur du vert n'est pas récupérée");
+        assertEquals(500,color.getGreen(),"la valeur du vert n'est pas récupérée");
     }
 
     @Test
     void testSetGreen(){
-        color1.setGreen(100);
+        color.setGreen(100);
         assertAll("objet non conforome",
-                () -> assertEquals(100,color1.getGreen(),"le get du vert apres son set n'a pas la bonne valeur "));
-        //assertEquals(100, color1.setGreen(100),"la valeur du vert n'a pas été setée");
+                () -> assertEquals(100,color.getGreen(),"le get du vert apres son set n'a pas la bonne valeur "));
+        //assertEquals(100, color.setGreen(100),"la valeur du vert n'a pas été setée");
     }
 
     @Test
     void testGetBlue() {
-        assertEquals(-1, color1.getBlue(), "la valeur du bleu n'est pas récupérée");
+        assertEquals(-1, color.getBlue(), "la valeur du bleu n'est pas récupérée");
     }
 
     @Test
     void testSetBlue(){
-        color1.setBlue(20);
+        color.setBlue(20);
         assertAll("object non conforme",
-               // () -> assertEquals(50, color1.setBlue(50),"la valeur du bleu n'a pas été setée"),
-                () -> assertEquals(20,color1.getBlue(),"le get du bleu apres son set n'a pas la bonne valeur "));
+               // () -> assertEquals(50, color.setBlue(50),"la valeur du bleu n'a pas été setée"),
+                () -> assertEquals(20,color.getBlue(),"le get du bleu apres son set n'a pas la bonne valeur "));
     }
 
     @Test
     void testGetHexavalue(){
-        assertEquals("0000FF", color2.getHexaValue(), "la valeur de l'hexa n'est pas récupérée");
+        assertEquals("0000FF", color.getHexValue(), "la valeur de l'hexa n'est pas récupérée");
     }
 
     @Test
     void testSetHexaValue(){
-        assertEquals("#00FF00", color2.setHexaValue("#00FF00"),"le get de l'hexa apres son set n'a pas la bonne valeur ");
+        assertEquals("#00FF00", color.setHexValue("#00FF00"),"le get de l'hexa apres son set n'a pas la bonne valeur ");
     }
 
     @Test
     void test_toString_Methode(){
-        assertEquals("value=#0000FF", color2.toString(),"le message de sortie n'est pas correct");
+        assertEquals("value=#0000FF", color.toString(),"le message de sortie n'est pas correct");
     }
 
 
